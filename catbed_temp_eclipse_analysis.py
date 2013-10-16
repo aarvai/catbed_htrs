ab3864578912876a3a172b19af97fb2b2cca570d
unknown <aarvai@ARVAI-LAPTOP.occ.harvard.edu>
2013-10-16 16:58:44 -0400

    Filtered dropouts,
    Filtering MUPS-1 and 2 dropouts made cleaner cooling profiles.  Required
    fetching as an msid set rather than by individual msid.  Also made
    start time generic.

--- catbed_temp_eclipse_analysis.py
+++ catbed_temp_eclipse_analysis.py
@@ -2,52 +2,65 @@ from kadi import events
 from Ska.engarchive import fetch_eng as fetch
 from Ska.Matplotlib import plot_cxctime
 
-from utilities import find_closest, find_last_before, find_first_after, same_limits
+from utilities import (find_closest, find_last_before, find_first_after, 
+                       same_limits, remove_therm_dropouts) 
 
 rcParams_orig = rcParams.copy()
 rcParams['lines.markersize'] = 3
 rcParams['axes.grid'] = True
+
+tstart = '2000:001'
 
 close('all')
 
 eclipses_t1 = array(events.eclipses.table['tstart'])
 eclipses_t2 = array(events.eclipses.table['tstop'])
 durs = eclipses_t2 - eclipses_t1
+msids = ['PM1THV1T','PM1THV2T','PM2THV1T','PM2THV2T',
+         'PM3THV1T','PM3THV2T','PM4THV1T','PM4THV2T']
+
+if 'msid_cache' not in globals():
+    print 'Fetching Telemetry'
+    tlm = fetch.Msidset(msids, tstart, stat='5min')
+    remove_therm_dropouts(tlm)
+    # Cache telemetry values between runs (use "run -i ...")
+    msid_cache = tlm 
+else:
+    print 'Getting telemetry from cache'
+    tlm = msid_cache
 
 marker_i = 0
-
-# Cache telemetry values between runs (use "run -i ...")
-if 'msid_cache' not in globals():
-    msid_cache = {}
 
 for thr in range(1,5):
 
     for ab, c in zip(range(1,3), ('b','r')):
 
         temp = 'PM' + str(thr) + 'THV' + str(ab) + 'T'
-
-        if temp not in msid_cache:
-            print 'Fetching {}'.format(temp)
-            x = fetch.Msid(temp, '2000:001')
-            x.select_intervals(events.eclipses)
-            msid_cache[temp] = x
-        else:
-            print 'Getting {} from cache'.format(temp)
-            x = msid_cache[temp]
+        
+        x = tlm[temp]
 
         eclipses_i1 = find_first_after(eclipses_t1, x.times)
         eclipses_i2 = find_last_before(eclipses_t2, x.times)
+        
+        # Check for eclipses with zero valid data due to dropouts
+        ok = eclipses_i2 > eclipses_i1
+        eclipses_i1 = eclipses_i1[ok]
+        eclipses_i2 = eclipses_i2[ok] 
+        
         eclipses_time0 = x.times[eclipses_i1]
         eclipses_temp0 = x.vals[eclipses_i1]
 
-        cooling_rates = (x.vals[eclipses_i2] - x.vals[eclipses_i1]) * 3600 / durs
+        cooling_rates = (x.vals[eclipses_i2] - x.vals[eclipses_i1]) * (3600 / 
+                        durs[ok])
         
-        figure(1)
-        subplot(2,2,thr)
-        x.plot(c + '.', label=temp)
-        title('MUPS-' + str(thr) + ' Valve Temps During Eclipses')
-        ylabel('deg F')
-        legend(loc='best')
+        #figure(1)
+        #subplot(2,2,thr)
+        #zipvals = zip(eclipses_i1, eclipses_i2)
+        #for i1, i2 in zipvals:
+        #    plot_cxctime(x.times[i1:i2], x.vals[i1:i2], c + '.')       
+        #title('MUPS-' + str(thr) + ' Valve Temps During Eclipses')
+        #ylabel('deg F')
+        #legend(loc='best')
 
         figure(2)
         subplot(2,2,thr)
@@ -60,7 +73,7 @@ for thr in range(1,5):
 
         figure(3)
         subplot(2,2,thr)
-        plot_cxctime(eclipses_t1, cooling_rates, c + '*', label=temp, mew=0)
+        plot_cxctime(eclipses_t1[ok], cooling_rates, c + '*', label=temp, mew=0)
         title('MUPS-' + str(thr) + ' Cooling Rates During Eclipses')
         ylabel('Cooling Rate [deg F/hr]')
         ylim([-100, 20])
@@ -86,9 +99,9 @@ for thr in range(1,5):
             starting_temps = x.vals[eclipses_i1]
             cooling_rates_corr = cooling_rates - (130 - starting_temps) * 10. / 30.
 
-            plot_cxctime(eclipses_t1, cooling_rates_corr, marker[marker_i], label=temp, mew=0)
+            plot_cxctime(eclipses_t1[ok], cooling_rates_corr, marker[marker_i], label=temp, mew=0)
             title('MUPS-1 and MUPS-2 Cooling Rates During Eclipses')
-            ylabel('Cooling Rate [deg F/hr]')
+            ylabel('Cooling Rate [deg F/hr] \n (Corrected for Eclipse Starting Temp)')
             ylim([-100, 20])
             grid()
             legend(loc='lower left')
@@ -97,13 +110,13 @@ for thr in range(1,5):
             plot(cooling_rates_corr, marker[marker_i], label=temp, mew=0)
             xlabel('Eclipse Number Since 2000:001')
             title('MUPS-1 and MUPS-2 Cooling Rates During Eclipses')
-            ylabel('Cooling Rate [deg F/hr]')
+            ylabel('Cooling Rate [deg F/hr] \n (Corrected for Eclipse Starting Temp)')
             ylim([-100, 20])
             legend(loc='lower left')
 
             marker_i = marker_i + 1
 
-for fig in range(1, 5):
+for fig in range(2, 5):
     figure(fig)
     subplot(221)
     axis1 = axis()
