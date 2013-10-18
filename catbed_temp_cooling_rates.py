05acc7adf8a7a8687669ab8b53ac8a96ea5a60be
unknown <aarvai@ARVAI-LAPTOP.occ.harvard.edu>
2013-10-18 19:15:46 -0400

    Filtered for thermistor dropouts, Corrected for starting temp

--- catbed_temp_cooling_rates.py
+++ catbed_temp_cooling_rates.py
@@ -1,105 +1,126 @@
+from scipy.optimize import curve_fit
 
 from kadi import events
+from Ska.engarchive import fetch_eng as fetch
+from Ska.Matplotlib import plot_cxctime
 
-from utilities import find_closest, find_last_before, find_first_after, same_limits
+from utilities import (find_closest, find_last_before, find_first_after, 
+                       same_limits, remove_therm_dropouts)
 
 close('all')
 
 tstart = '2000:001'
-pitch_bin_min = 156
-pitch_bin_max = 166
+pitch_bin_min = 160
+pitch_bin_max = 170
+min_dur = 120 #minutes
 
-all_dwells_t1 = array(events.dwells.table['tstart'])
-all_dwells_t2 = array(events.dwells.table['tstop'])
-all_durs = array(events.dwells.table['dur'])
-all_dwells_tmid = all_dwells_t1 + .5 * all_durs
+dwells = events.dwells.filter(dur__gt=min_dur*60)
+
+all_t1 = array(dwells.table['tstart'])
+all_t2 = array(dwells.table['tstop'])
+all_durs = array(dwells.table['dur'])
+all_tmid = all_t1 + .5 * all_durs
 
 x = fetch.Msid('PITCH', tstart, stat='5min')
-all_pitch_tmid_i = find_closest(all_dwells_tmid, x.times)
-all_pitch_tmid = x.vals[all_pitch_tmid_i]
+all_pitch_i = find_closest(all_tmid, x.times)
+all_pitch = x.vals[all_pitch_i]
+# Identify dwells without valid pitch data
+ok = ((x.times[all_pitch_i] > all_t1) & 
+      (x.times[all_pitch_i] < all_t2))     
+if sum(~ok) > 0:
+    print('Pitch data missing during ' + str(sum(~ok)) + ' dwells')
+    
+in_bin = (all_pitch > pitch_bin_min) & (all_pitch < pitch_bin_max)
+t1 = all_t1[in_bin & ok]
+t2 = all_t2[in_bin & ok]
+pitch = all_pitch[in_bin & ok]
+durs = all_durs[in_bin & ok]
 
-in_bin = (all_pitch_tmid > pitch_bin_min) & (all_pitch_tmid < pitch_bin_max)
-t1 = all_dwells_t1[in_bin]
-t2 = all_dwells_t2[in_bin]
-pitch = all_pitch_tmid[in_bin]
-durs = all_durs[in_bin]
+msids = ['PM1THV1T','PM1THV2T','PM2THV1T','PM2THV2T',
+         'PM3THV1T','PM3THV2T','PM4THV1T','PM4THV2T']
 
-#marker_i = 0
+if 'msid_cache' not in globals():
+    print 'Fetching Telemetry'
+    tlm = fetch.Msidset(msids, tstart, stat='5min')
+    remove_therm_dropouts(tlm)
+    # Cache telemetry values between runs (use "run -i ...")
+    msid_cache = tlm 
+else:
+    print 'Getting telemetry from cache'
+    tlm = msid_cache
+    
+def linfit(x, a, b):
+    return a + b*x 
 
 for thr in range(1,3):
 
     for ab, c in zip(range(1,3), ('b','r')):
 
         temp = 'PM' + str(thr) + 'THV' + str(ab) + 'T'
-        x = fetch.Msid(temp, '2000:001', stat='5min')
-
+        x = tlm[temp]
+        
         i1 = find_first_after(t1, x.times)
         i2 = find_last_before(t2, x.times)
         
-        cooling_rates = (x.vals[i2] - x.vals[i1]) * 3600 / durs
+        # Check for eclipses with zero valid data due to dropouts
+        ok2 = i2 > i1
+        i1 = i1[ok2]
+        i2 = i2[ok2] 
+        
+        cooling_rates = (x.vals[i2] - x.vals[i1]) * 3600 / durs[ok2]
+        
+        # Correct cooling rates for starting temperature
+        # Linear fit varies by valve and by temp
+        p_opt, p_cov = curve_fit(linfit, float64(x.vals[i1]), cooling_rates)
+        corr_rates = linfit(float64(x.vals[i1]), p_opt[0], p_opt[1])
                 
-        #figure(1)
-        #subplot(1,2,thr)
-        #zipvals = zip(i1, i2)
-        #for i1i, i2i in zipvals:
-        #    plot_cxctime(x.times[i1i:i2i], x.vals[i1i:i2i], c+'.', label=temp)
-        #title('MUPS-' + str(thr) + ' Valve Temps during Dwells at 156 - 166 Pitch')
-        #ylabel('deg F')
-
-        #figure(2)
-        #subplot(1,2,thr)
-        #zipvals = zip(i1, i2)
-        #for i1i, i2i in zipvals:
-        #    plot(x.times[i1i:i2i] - x.times[i1i], x.vals[i1i:i2i] - x.vals[i1i], c, label=temp)
-        #title('Change in MUPS-' + str(thr) + 'Temps During Dwells at 156 - 166 Pitch')
-        #xlabel('Time into Dwell [sec]')
-        #ylabel('deg F')
-          
-        figure(3)
+        figure(1)
         subplot(1,2,thr)
-        plot_cxctime(t1 + .5*durs, cooling_rates, c + '*', label=temp, mew=0)
-        title('MUPS-' + str(thr) + ' Cooling Rates During Dwells at 156 - 166 Pitch')
-        ylabel('Cooling Rate [deg F/hr]')
+        plot_cxctime(t1[ok2] + .5*durs[ok2], cooling_rates, c + '*', 
+                     label=temp, mew=0, alpha=.1)
+        plot_cxctime(t1[ok2] + .5*durs[ok2], cooling_rates, c + '*', 
+                     label=temp, mew=0, alpha=.1)
+        title('MUPS-' + str(thr) + ' Cooling Rates During Dwells at ' + 
+              str(pitch_bin_min) + ' - ' + str(pitch_bin_max) + ' Pitch')
+        ylabel('Uncorrected Cooling Rate [deg F/hr] \n (Filtered for Dwells > ' 
+               + str(min_dur) + ' min)')
         #ylim([-100, 20])
         legend(loc='best')
 
-        #figure(4)
-        #subplot(1,2,thr)
-        #plot(x.vals[eclipses_i1], cooling_rates, c + '*', label=temp, mew=0)
-        #title('MUPS-' + str(thr) + ' Cooling Rates During Eclipses vs Starting Temp')
-        #ylabel('Cooling Rate [deg F/hr]')
-        #xlabel('Eclipse Starting Temp [deg F]')
+        figure(2)
+        subplot(1,2,thr)
+        plot(x.vals[i1], cooling_rates, c + '*', label=temp, mew=0)
+        plot(xlim(), linfit(array(xlim()), p_opt[0], p_opt[1]), c + ':', alpha=.1)
+        plot(xlim(), linfit(array(xlim()), p_opt[0], p_opt[1]), c + ':', alpha=.1)
+        title('MUPS-' + str(thr) + 
+              ' Cooling Rates During Eclipses vs Starting Temp')
+        ylabel('Uncorrected Cooling Rate [deg F/hr]')
+        xlabel('Dwell Starting Temp [deg F] \n (Filtered for Dwells > ' 
+               + str(min_dur) + ' min & btwn ' + str(pitch_bin_min) + 
+               ' - ' + str(pitch_bin_max) + ' Pitch)')
         #ylim([-100, 20])
-        #legend(loc='best')
+        legend(loc='best')
         
-        #if thr == 1 or thr == 2:
-        #    marker = ['co','cs', 'mo', 'ms']
-        #
-        #    figure(5)
-        #    plot_cxctime(eclipses_t1, cooling_rates, marker[marker_i], label=temp, mew=0)
-        #    title('MUPS-1 and MUPS-2 Cooling Rates During Eclipses')
-        #    ylabel('Cooling Rate [deg F/hr]')
-        #    ylim([-100, 20])
-        #    legend(loc='lower left')
+        figure(3)
+	subplot(1,2,thr)
+	plot_cxctime(t1[ok2] + .5*durs[ok2], corr_rates, c + '*', label=temp, 
+	             mew=0, alpha=.1)
+	plot_cxctime(t1[ok2] + .5*durs[ok2], corr_rates, c + '*', label=temp, 
+	             mew=0, alpha=.1)
+	title('MUPS-' + str(thr) + ' Cooling Rates During Dwells at ' + 
+	      str(pitch_bin_min) + ' - ' + str(pitch_bin_max) + 
+	      ' Pitch \n Corrected for Starting Temps')
+	ylabel('Cooling Rate (Corrected for Starting Temp) [deg F/hr] \n (Filtered for Dwells > ' 
+	       + str(min_dur) + ' min)')
+	#ylim([-100, 20])
+        legend(loc='best')
         
-        #    figure(6)
-        #    plot(cooling_rates, marker[marker_i], label=temp, mew=0)
-        #    xlabel('Eclipse Number Since 2000:001')
-        #    title('MUPS-1 and MUPS-2 Cooling Rates During Eclipses')
-        #    ylabel('Cooling Rate [deg F/hr]')
-        #    ylim([-100, 20])
-        #    legend(loc='lower left')
-
-        #    marker_i = marker_i + 1
-
 for fig in range(1, 4):
     figure(fig)
     subplot(121)
     axis1 = axis()
     subplot(122)
-    axis(axis1)
-    if fig < 4:
-        tight_layout()      
+    axis(axis1)     
 
     
     
