f6efe9a357de920035975fef5fa702983e0abe54
unknown <aarvai@ARVAI-LAPTOP.occ.harvard.edu>
2013-10-31 15:04:31 -0400

    Added Duty Cycle Computations
    Would still like to filter dumps

--- catbed_temp_htr_dc.py
+++ catbed_temp_htr_dc.py
@@ -3,11 +3,14 @@ from utilities import append_to_array, find_first_after, same_limits, heat_map
 
 close('all')
 
-temp = 'PM3THV1T'
-on_range = 65
-off_range = 90 
+temp = 'PM3THV2T'
+on_range = 60
+off_range = 89 
+t_start = '2000:001'
+t_stop = None
+#t_stop = '2013:268'
 
-x = fetch.Msid(temp, '2000:001', '2013:286')
+x = fetch.Msid(temp, t_start, t_stop)
 dt = diff(x.vals)
 
 local_min = (append_to_array(dt <= 0., pos=0, val=bool(0)) & 
@@ -21,21 +24,40 @@ htr_off_range = x.vals > off_range
 htr_on = local_min & htr_on_range
 htr_off = local_max & htr_off_range
 
+#remove any incomplete heater cycles at end of timeframe
+last_off = nonzero(htr_off)[0][-1]
+htr_on[last_off:] = 0
+
 t_on = x.times[htr_on]
 t_off = x.times[htr_off]
 
-#over = nonzero(append_to_array(t_off[:-1] > t_on[1:], pos=-1, val=bool(0)))
-#t_off[over] = t_on[over] + .5*(t_on[over+1] - t_on[over])
-#t_off = [t_off[i] = t_on[i] + .5*(t_on[i+1] - t_on[i]) for i in range(len(t_on)) if t_off[i] > t_on[i+1]]
-
 match_i = find_first_after(t_on, t_off)
-
 
 dur = t_off[match_i] - t_on
 
+#compute duty cycles by month
+on_dates = DateTime(t_on).iso
+on_yrs = [date[0:4] for date in on_dates]
+on_mos = [date[5:7] for date in on_dates]
+on_freq = zeros(168)
+on_time = zeros(168)
+dates = zeros(168)
+i = 0
+for yr in range(2000, 2014):
+    for mo in range(1,13):
+        yr_match = array([on_yr == str(yr) for on_yr in on_yrs])
+        mo_match = array([on_mo == str(mo).zfill(2) 
+                          for on_mo in on_mos])
+        on_freq[i] = sum(yr_match & mo_match)
+        on_time[i] = sum(dur[yr_match & mo_match])
+        dates[i] = DateTime(str(yr) + '-' + str(mo).zfill(2) 
+                            + '-01 00:00:00.000').secs
+        i = i + 1
+dates_range = append(dates, DateTime('2014:001').secs)
+dc = on_time / (dates_range[1:] - dates_range[:-1])
+
 figure(1)
-plot_cxctime(t_on, dur, 'b.', alpha=.2)
-plot_cxctime(t_on, dur, 'b.', alpha=.2)
+plot_cxctime(t_on, dur, 'b.', alpha=1)
 ylabel('On-Time Durations [sec]')
 title('MUPS-3 Valve Heater Cycle Durations')
 
@@ -46,7 +68,20 @@ plot_cxctime(x.times[htr_on], x.vals[htr_on], 'c*',mew=0)
 plot_cxctime(x.times[htr_off], x.vals[htr_off], 'r*',mew=0)
 
 figure(3)
-heat_map(t_on, dur, y_lim=[0,2000], bins=400)
-ylabel('On-Time Durations [sec]')
-title('MUPS-3 Valve Heater Cycle Durations')
-xlabel('Time since 2000:001 [sec]')
+hist(dur, bins=100, normed=True)
+xlabel('On-Time Durations [sec]')
+
+figure(4)
+plot_cxctime(dates, dc*100, '*')
+title('MUPS-3 Valve Heater Duty Cycle')
+ylabel('Heater Duty Cycle by Month [%]')
+
+figure(5)
+plot_cxctime(dates, on_freq, '*')
+title('MUPS-3 Valve Heater Cycling Frequency')
+ylabel('Heater Cycles per Month')
+
+figure(6)
+plot_cxctime(dates, on_time/3600, '*')
+title('MUPS-3 Valve Heater On-Time')
+ylabel('Heater On-Time by Month [hrs]')
